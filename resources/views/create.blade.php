df02d3d6181e011a7a5cdcd4a48045d2f5dfea16
Vincenzo Tito <vincenzotito@icloud.com>
2024-02-28 18:27:16 +0100

    migliorie

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,17 +1,14 @@
 <x-Layout>
 
     <x-slot:title>Inserisci | Libro</x-slot>
+    <x-success/>
+    <x-delete/>
     <x-back />
+    
     <div
         style="background-color:rgba(255, 255, 255, 0.693); width:900px;height:400px;position: absolute;top:120px; left:200px">
         <h2 style="font-size: 50px;color:#811f1f;text-shadow:2px 2px 2px black;" class="text-center">Nuovo Libro</h2>
-        @if (session()->has('success'))
-            <h4 class="alert alert-success">{{ session('success') }}</h4>
-        @endif
-
-        @if (session()->has('error'))
-            <h4 class="alert alert-danger">{{ session('error') }}</h4>
-        @endif
+    
         <div class="col-lg-8 mx-auto">
             <form style=" padding:10px; margin-top:4px;" action="{{ route('books.store') }}" method="POST"
                 enctype="multipart/form-data">
